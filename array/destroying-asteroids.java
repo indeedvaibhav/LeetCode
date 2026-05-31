c05b260b8c2d49fc22104365a2e6b28100a91527
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-01 01:01:27 +0530

    LC-destroying-asteroids [Runtime: N/A; Memory: N/A]

--- array/destroying-asteroids.java
+++ array/destroying-asteroids.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         int massgain = 0;
+        Long Mass=mass;
         boolean flag = false;
         Arrays.sort(asteroids);
 
         for (int i = 0; i < asteroids.length; i++)
         {
-            if (mass >= asteroids[i]) {
+            if (Mass >= asteroids[i]) {
                 massgain = asteroids[i];
-                mass += massgain;
+                Mass += massgain;
                 flag=true;
             } 
             else
