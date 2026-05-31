51a4beec6ea28459fc78143a8da251b74228a6cf
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-01 00:43:03 +0530

    LC-destroying-asteroids [Runtime: N/A; Memory: N/A]

--- greedy/destroying-asteroids.java
+++ greedy/destroying-asteroids.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         int massgain=0;
         boolean flag=false;
-        for(int i=0;i<asteroid.length;i++)
+        for(int i=0;i<asteroids.length;i++)
         {
             if(mass>=asteroid[i])
             {
-                massgain=asteroid[i];
+                massgain=asteroids[i];
                 mass+=massgain;
             }
             else
