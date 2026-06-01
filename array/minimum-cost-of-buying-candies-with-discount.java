be7ed6617377b2e7ee7503d353548dad16212e3a
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-01 13:24:03 +0530

    LC-minimum-cost-of-buying-candies-with-discount [Runtime: N/A; Memory: N/A]

--- array/minimum-cost-of-buying-candies-with-discount.java
+++ array/minimum-cost-of-buying-candies-with-discount.java
@@ -1,24 +1,16 @@
 class Solution {
     public int minimumCost(int[] cost) {
-        int total=0;
-        int i=0;
-        if(cost.length==1) return cost[0];
-        if(cost.length==2) return (cost[0]+cost[1]);
-        cost = Arrays.stream(cost)
-                .boxed()
-                .sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue)
-                .toArray();
-        while(i<cost.length && (i+1)<cost.length)
-        {
-            int sum= cost[i]+cost[i+1];
-            total=total+sum;    
-            i+=3;
+        Arrays.sort(cost);
+
+        int total = 0;
+
+        for (int i = cost.length - 1; i >= 0; i -= 3) {
+            total += cost[i];
+
+            if (i - 1 >= 0)
+                total += cost[i - 1];
         }
-        while(i<cost.length) {
-            total = total + cost[i];
-            i++;
-        }
+
         return total;
 
     }
