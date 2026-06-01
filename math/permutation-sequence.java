a1f16ae0717a3b59c5515a9dd141e0e54db8029c
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 01:14:02 +0530

    LC-permutation-sequence [Runtime: N/A; Memory: N/A]

---
+++ math/permutation-sequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        String p="";String up="";
+        for(int i=0;i<n;i++)
+        up=up+i;
+        return permutationIndex(p,up,k);
+        
+    }
+    static String permutationIndex(String p,String up,int k)
+    {
+        if(up.isEmpty())
+        return p;
+
+        char ch=up.charAt(0);
+        
+        ArrayList<String> ans= new ArrayList<>();
+        for(int i=0;i<=p.length;i++)
+        {
+            String first=p.substring(0,i);
+            String second=p.substring(i,p.length());
+            ans.addAll(permutationIndex(first+ch+second,up.substring(1)));
+        }
+        String result=ans.get(k);
+        return result;
+    }
+}
