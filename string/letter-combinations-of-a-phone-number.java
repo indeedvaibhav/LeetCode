fdc3bb6fb79cf512831f6419ff246756b1e32a1c
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 13:34:35 +0530

    LC-letter-combinations-of-a-phone-number [Runtime: N/A; Memory: N/A]

--- string/letter-combinations-of-a-phone-number.java
+++ string/letter-combinations-of-a-phone-number.java
@@ -17,7 +17,7 @@ class Solution {
         for(int i=(digit-1)*3;i<digit*3;i++)
         {
             char ch=(char)('a'+i);
-            ans.addAll(pad(p+ch,up.substring(1)));
+            ans.addAll(combinations(p+ch,up.substring(1)));
         }
         return ans;
     }
