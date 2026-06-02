c687a99446d93a31e251e82e96c13cae4cc00720
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 22:03:30 +0530

    LC-finding-3-digit-even-numbers [Runtime: N/A; Memory: N/A]

--- hash-table/finding-3-digit-even-numbers.java
+++ hash-table/finding-3-digit-even-numbers.java
@@ -1,44 +1,33 @@
 class Solution {
-    HashSet<Integer> ans = new HashSet<>();
-    boolean[] unused;
     public int[] findEvenNumbers(int[] digits) {
-        String s = "";
-        
-        for (int i = 0; i < digits.length; i++)
-            unused[i] = false;
-        int[] result = solve(digits, s);    
-        return result;
+        HashSet<Integer> Set= new HashSet<>();
+        boolean[] used= new boolean[digits.length];
 
+        backtracking(digits,used,"",set);
+        int[] ans= set.stream().mapToInt(Integer::intValue).sorted().toArray();
+
+        return ans;
     }
- 
-    int[] solve(int[] digits, String current, int[] unused) {
-        if(current.length()==3)
+    static int backtracking(int[] digits,boolean[] used,String current,HashSet<Integer> set)
+    {
+        if(current.length==3)
         {
-            int number = Integer.parseInt(current); 
-            if(number>=100 && number%2==0)
-            {
-                ans.add(number); 
-            }
+            int num=Integer.parseInt(current);
+            if(current.charAt(0)=='0' && num%2==0)
+            set.add(num);
         }
-
-        for(int i=0;i<digits.length;i++)
-        {
-            if(unused[i]==true)
-            continue;
-
-            current=current+ digits[i];
-            unused[i]= true;
-
-            solve(digits, current, unused);
-            unused[i]=false;
-            current.substring(current.length-1-i);
-        }
-        int[] array = new int[set.size()];
-        int index = 0;
-        for (Integer num : set) 
-        {
-            array[index++] = num; // Auto-unboxing handles conversion
-        }
-        return array;
+        return;
     }
+    for(int i=0;i<digits.length;i++)
+    {
+        if(used[i]) continue;
+
+        used[i]= true;
+        backtracking(digits,used,current+digits[i],set);
+        used[i]=false;
+    }
+
+    
+}
+
 }
