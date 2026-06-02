00d2c4a256d9d4051ab271e1341ce6779d76cbf8
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 13:55:26 +0530

    LC-letter-combinations-of-a-phone-number [Runtime: N/A; Memory: N/A]

--- string/letter-combinations-of-a-phone-number.java
+++ string/letter-combinations-of-a-phone-number.java
@@ -15,7 +15,7 @@ class Solution {
         ArrayList<String> ans = new ArrayList<>();
         int digit = up.charAt(0) - '1';
         if (digit == 6 || digit == 8) {
-            for (int i = (digit - 1) * 3; i <= (digit * 3); i++) {
+            for (int i = (digit - 1) * 3; i < (digit * 3)+1; i++) {
                 char ch = (char) ('a' + i);
                 ans.addAll(combinations(p + ch, up.substring(1)));
             }
