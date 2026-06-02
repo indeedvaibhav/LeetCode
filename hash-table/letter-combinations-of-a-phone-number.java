5dba638952561ff998ae4637574e198e30a9f74e
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 13:30:40 +0530

    LC-letter-combinations-of-a-phone-number [Runtime: N/A; Memory: N/A]

---
+++ hash-table/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String p="";
+        return combinations(p,s);
+        
+    }
+    static ArrayList<String> combinations(String p, Strinp up)
+    {
+        if(up.isEmpty())
+        {
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans=new ArrayList<>();
+        int digit=up.charAt(0)-'0';
+        for(int i=(digit-1)*3;i<(digit*3);i++)
+        {
+            char ch=(char)('a'+i);
+            ans.addAll(combinations(p+ch,up.substring(1)));
+        }
+        return ans;
+    }
+}
