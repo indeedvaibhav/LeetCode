5e56c7051b51d66722cb182940609e15745b47c6
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 15:55:43 +0530

    LC-earliest-finish-time-for-land-and-water-rides-i [Runtime: N/A; Memory: N/A]

---
+++ greedy/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int earliest1 = Integer.MAX_VALUE;
+        int count = 0;
+        for (int i = 0; i < landStartTime.length; i++) {
+            int endtime = landStartTime[i] + landDuration[i];
+            for (int j = 0; j < waterStartTime.length; j++) {
+                endtime += waterDuration[j];
+            }
+            if (endtime < earliest)
+                earliest1 = endtime;
+            endtime = 0;
+        }
+        int earliest2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < waterStartTime.length; i++) {
+            int endtime = waterStartTime[i] + waterDuration[i];
+            for (int j = 0; j < landStartTime.length; j++) {
+                endtime += landDuration[i];
+                if (endtime < earliest)
+                    earliest2 = endtime;
+                endtime -= landDuration[i];
+            }
+            if (earlist1 < earliest2)
+                return earliest1;
+
+            return earliest2;
+        }
+    }
+}
