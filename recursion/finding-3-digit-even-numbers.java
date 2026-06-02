815f71d7c2796d3e1ba3b5213803873d3efbe537
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 22:04:37 +0530

    LC-finding-3-digit-even-numbers [Runtime: N/A; Memory: N/A]

--- recursion/finding-3-digit-even-numbers.java
+++ recursion/finding-3-digit-even-numbers.java
@@ -1,33 +1,31 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        HashSet<Integer> Set= new HashSet<>();
-        boolean[] used= new boolean[digits.length];
+        HashSet<Integer> Set = new HashSet<>();
+        boolean[] used = new boolean[digits.length];
 
-        backtracking(digits,used,"",set);
-        int[] ans= set.stream().mapToInt(Integer::intValue).sorted().toArray();
+        backtracking(digits, used, "", set);
+        int[] ans = set.stream().mapToInt(Integer::intValue).sorted().toArray();
 
         return ans;
     }
-    static int backtracking(int[] digits,boolean[] used,String current,HashSet<Integer> set)
-    {
-        if(current.length==3)
-        {
-            int num=Integer.parseInt(current);
-            if(current.charAt(0)=='0' && num%2==0)
-            set.add(num);
+
+    static int backtracking(int[] digits, boolean[] used, String current, HashSet<Integer> set) {
+        if (current.length == 3) {
+            int num = Integer.parseInt(current);
+            if (current.charAt(0) == '0' && num % 2 == 0) {
+                set.add(num);
+            }
+            return;
         }
-        return;
-    }
-    for(int i=0;i<digits.length;i++)
-    {
-        if(used[i]) continue;
+        for (int i = 0; i < digits.length; i++) {
+            if (used[i])
+                continue;
 
-        used[i]= true;
-        backtracking(digits,used,current+digits[i],set);
-        used[i]=false;
-    }
+            used[i] = true;
+            backtracking(digits, used, current + digits[i], set);
+            used[i] = false;
+        }
 
-    
-}
+    }
 
 }
