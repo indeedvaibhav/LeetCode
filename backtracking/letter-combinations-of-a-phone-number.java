1f60ffa445e95af170be658dad6c4eeef7adee25
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 13:34:13 +0530

    LC-letter-combinations-of-a-phone-number [Runtime: N/A; Memory: N/A]

--- backtracking/letter-combinations-of-a-phone-number.java
+++ backtracking/letter-combinations-of-a-phone-number.java
@@ -6,18 +6,18 @@ class Solution {
     }
     static ArrayList<String> combinations(String p, String up)
     {
-        if(up.isEmpty())
-        {
+        if(up.isEmpty()) {
             ArrayList<String> list=new ArrayList<>();
             list.add(p);
-            return list;
+            return p;
+           
         }
         ArrayList<String> ans=new ArrayList<>();
-        int digit=up.charAt(0)-'0';
-        for(int i=(digit-1)*3;i<(digit*3);i++)
+        int digit= up.charAt(0)-'0';
+        for(int i=(digit-1)*3;i<digit*3;i++)
         {
             char ch=(char)('a'+i);
-            ans.addAll(combinations(p+ch,up.substring(1)));
+            ans.addAll(pad(p+ch,up.substring(1)));
         }
         return ans;
     }
