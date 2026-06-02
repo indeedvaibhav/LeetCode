6a451a8081f2b68b4bb7390f33bff8682077b53c
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-02 13:52:39 +0530

    LC-letter-combinations-of-a-phone-number [Runtime: N/A; Memory: N/A]

--- hash-table/letter-combinations-of-a-phone-number.java
+++ hash-table/letter-combinations-of-a-phone-number.java
@@ -1,23 +1,28 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        String p="";
-        return combinations(p,digits);
-        
+        String p = "";
+        return combinations(p, digits);
+
     }
-    static ArrayList<String> combinations(String p, String up)
-    {
-        if(up.isEmpty()) {
-            ArrayList<String> list=new ArrayList<>();
+
+    static ArrayList<String> combinations(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
-           
+
         }
-        ArrayList<String> ans=new ArrayList<>();
-        int digit= up.charAt(0)-'1';
-        for(int i=(digit-1)*3;i<(digit*3)+1;i++)
-        {
-            char ch=(char)('a'+i);
-            ans.addAll(combinations(p+ch,up.substring(1)));
+        ArrayList<String> ans = new ArrayList<>();
+        int digit = up.charAt(0) - '1';
+        if (digit == 7 || digit == 9) {
+            for (int i = (digit - 1) * 3; i <= (digit * 3); i++) {
+                char ch = (char) ('a' + i);
+                ans.addAll(combinations(p + ch, up.substring(1)));
+            }
+        }
+        for (int i = (digit - 1) * 3; i < (digit * 3); i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(combinations(p + ch, up.substring(1)));
         }
         return ans;
     }
