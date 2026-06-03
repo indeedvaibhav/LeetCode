a07225bc8c1ec658d1179a9101263a158ea0b070
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-03 12:56:01 +0530

    LC-earliest-finish-time-for-land-and-water-rides-ii [Runtime: N/A; Memory: N/A]

---
+++ sorting/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+         int landFirst = calculate(
+                landStartTime, landDuration,
+                waterStartTime, waterDuration
+        );
+
+        int waterFirst = calculate(
+                waterStartTime, waterDuration,
+                landStartTime, landDuration
+        );
+
+        return Math.min(landFirst, waterFirst);
+    }
+    public int earliest(int[] firstStartTime, int[] firstDuration, int[]secondStartTime,int[] secondduration)
+    {
+        int earliesttime=Integer.MAX_VALUE;
+        for(int i=0;i<firstStartTime;i++)
+        {
+            earliesttime=Math.min(earliesttime,firstStart[i]+firstDuration[]);
+        }
+        int result=Integer.MAX_VALUE;
+        for(int i=0;i<secondStartTime;i++)
+        {
+            int secondstart=Math.max(secondStartTime[i],earliesttime);
+
+            result = Math.min(
+                    result,
+                    startSecond + secondDuration[i]
+            );
+        }
+        return result;
+    }
+} 
