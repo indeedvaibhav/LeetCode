19c377176c18a3b4ebf3ef6775006dcb47186e10
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-04 00:48:41 +0530

    LC-elimination-game [Runtime: N/A; Memory: N/A]

--- recursion/elimination-game.java
+++ recursion/elimination-game.java
@@ -1,36 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
+class Solution {
+    public int lastRemaining(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
 
-public class elimination {
-    public static void main(String[] args) {
-        int n=9;
-        ArrayList<Integer> numbers=new ArrayList<>();
-        for(int i=1;i<=n;i++)
-            numbers.add(i);
-        System.out.println(solve(numbers,true));
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
 
+        return solve(list, true);
     }
 
-    static int solve(ArrayList<Integer> list,boolean leftRight)
-    {
-        if(list.size()==1)
+    static int solve(ArrayList<Integer> list, boolean leftRight) {
+        if (list.size() == 1) {
             return list.get(0);
-        ArrayList<Integer> ans=new ArrayList<>();
-
-        if(leftRight){
-            for(int i=1;i<list.size();i+=2)
-            {
-                ans.add(list.get(i));
-            }
         }
-        else
-        {
-            for(int i=list.size()-2;i>=0;i-=2)
-            {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (leftRight) {
+
+            for (int i = 1; i < list.size(); i += 2) {
                 ans.add(list.get(i));
             }
+
+        } else {
+
+            for (int i = list.size() - 2; i >= 0; i -= 2) {
+                ans.add(list.get(i));
+            }
+
             Collections.reverse(ans);
         }
-        return solve(ans,!leftRight);
+
+        return solve(ans, !leftRight);
     }
 }
