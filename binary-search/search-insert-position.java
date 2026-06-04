704f8933904f16a74505a4a09807e17c46f35b49
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-04 16:40:11 +0530

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

--- binary-search/search-insert-position.java
+++ binary-search/search-insert-position.java
@@ -1,25 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int l=0,u=nums.length-1,found=0,m=0;
-        while(l<=u)
-        {
-            m=u-(l+u)/2;
-            if(target>nums[m])
-            {u=m-1;}
-            else if(target<nums[m])
-            l=m+1;
-            else 
-            {
-                found=1;
-                break;
-            }
-            
+        int l = 0;
+        int u = nums.length - 1;
+
+        while (l <= u) {
+            int m = l + (u - l) / 2;
+
+            if (nums[m] == target)
+                return m;
+
+            if (nums[m] < target)
+                l = m + 1;
+            else
+                u = m - 1;
         }
-        if(found==1)
-        return m;
-        else
-        
-        
-        
+
+        return l;
     }
 }
