85f8cd403b257e96c2801266badb86d9d4d1611e
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-05 11:15:33 +0530

    LC-total-waviness-of-numbers-in-range-ii [Runtime: N/A; Memory: N/A]

---
+++ math/total-waviness-of-numbers-in-range-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long totalWaviness(long num1, long num2) {
+        for(int i=num1;i<=num2;i++)
+        {
+            String s=String.valueOf(i);
+            for(int j=1;j<s.length();j++)
+            {
+                char left= s.charAt(j-1);
+                char left= s.charAt(j-1);
+
+            }
+        }
+        
+    }
+}
