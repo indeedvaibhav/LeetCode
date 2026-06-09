91afd52e2fd1dedad6ccd124bbd521e6f7b4fa70
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-09 22:08:31 +0530

    LC-maximum-total-subarray-value-i [Runtime: N/A; Memory: N/A]

--- array/maximum-total-subarray-value-i.java
+++ array/maximum-total-subarray-value-i.java
@@ -1,17 +1,9 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
         int i=0;
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        while(i<nums.length)
-        {
-            if(nums[i]<min)
-{            min=nums[i];
-}            if(nums[i]>max)
-{            max=nums[i];
-}            i++;
-        }
-        return (max-min)*k;
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+       return (max-min)*k;
         
     }
 }
