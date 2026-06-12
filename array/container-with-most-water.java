5207665e3664b6d7a574a876c7fab1f43d28efd4
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-12 23:17:45 +0530

    LC-container-with-most-water [Runtime: 4 ms; Memory: 77.4 MB]

---
+++ array/container-with-most-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int start=0,end=height.length-1;
+        int max=Integer.MIN_VALUE;
+        while(start<end)
+        {
+            if(height[start]<height[end])
+            {
+                max=Math.max(max,height[start]*(end-start));
+                start++;
+            }
+            else if(height[start]>height[end])
+            {
+                max=Math.max(max,height[end]*(end-start));
+                end--;
+            }
+            else {
+                max = Math.max(max, height[start] * (end - start));
+                start++;
+                end--;
+            }
+        }
+        return max;
+    }
+}
