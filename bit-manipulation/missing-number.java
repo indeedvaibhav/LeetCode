7b477d7609a9798ad038621a34d7d557358d108f
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-14 02:35:36 +0530

    LC-missing-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/missing-number.java
+++ bit-manipulation/missing-number.java
@@ -1,20 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-       HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
-            map.put(nums[i],i);
+        int xor = nums.length;
 
-        int ans=0;
-        for(int i=0;i<=map.size();i++)
-        {
-            if(!map.containsKey(i)) {
-                ans=i;
-                break;
-
-            }
-            else
-                continue;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
         }
-        return ans; 
+
+        return xor;
     }
 }
