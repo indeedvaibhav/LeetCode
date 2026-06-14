556bae2ffca35e8446c8289512c215a0960c24ad
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-14 13:31:11 +0530

    LC-intersection-of-two-arrays-ii [Runtime: 3 ms; Memory: 45.2 MB]

---
+++ two-pointers/intersection-of-two-arrays-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                resultList.add(num);
+                map.put(num, map.get(num) - 1); 
+            }
+        }
+
+        int[] resultArray = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+
+        return resultArray;
+        
+    }
+}
