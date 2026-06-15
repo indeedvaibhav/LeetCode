b149517133e4e7208468be7c0e23b9679b1ecf80
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-15 22:06:05 +0530

    LC-island-perimeter [Runtime: N/A; Memory: N/A]

--- breadth-first-search/island-perimeter.java
+++ breadth-first-search/island-perimeter.java
@@ -1,21 +1,27 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-       int peri=0;
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            {
-                if(grid[i][j]==1)
-                    peri+=4;
+       static int islandPeri(int[][] grid) {
+    int peri = 0;
 
+    for (int i = 0; i < grid.length; i++) {
+        for (int j = 0; j < grid[0].length; j++) {
+
+            if (grid[i][j] == 1) {
+                peri += 4;
+
+                // Shared edge with top cell
                 if (i > 0 && grid[i - 1][j] == 1)
                     peri -= 2;
 
+                // Shared edge with left cell
                 if (j > 0 && grid[i][j - 1] == 1)
                     peri -= 2;
             }
         }
-        return peri;
+    }
+
+    return peri;
+}
 
     }
 }
