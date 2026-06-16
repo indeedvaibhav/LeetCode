5ca987b1ed30366131caeb45f963f78b415257ab
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-16 14:28:45 +0530

    LC-relative-ranks [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/relative-ranks.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        HashMap<Integer , String> map=new HashMap<>();
+        int[] sorted= score.clone();
+        Arrays.sort(sorted);
+        String[] result= new String[sorted.length]; int len=sorted.length-1;
+        map.put(sorted[len],"Gold Medal");
+        map.put(sorted[len-1],"Silver Medal");
+        map.put(sorted[len-2],"Bronze Medal");
+        for(int i=3;i<sorted.length;i++)
+            map.put(sorted[len-i],(i+1)+"");
+
+
+        for(int i=0;i<=score.length-1;i++)
+        {
+            result[i]=map.get(score[i]);
+
+        }
+        return result;
+    }
+}
