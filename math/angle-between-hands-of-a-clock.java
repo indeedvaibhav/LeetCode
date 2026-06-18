b87291c215d1a73d95e4d4151b01734beaae4ae9
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-19 00:35:47 +0530

    LC-angle-between-hands-of-a-clock [Runtime: N/A; Memory: N/A]

--- math/angle-between-hands-of-a-clock.java
+++ math/angle-between-hands-of-a-clock.java
@@ -1,24 +1,14 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=1;i<=12;i++)
-            map.put(5*i,30*i);
 
-        double hourAffected= minutes*0.5;
-        double degree=0;
-        if(minutes==0 ){
-            if(hour==12)
-             return 0;
-            else
-                degree=map.get(hour * 5);
-        }
-        else {
-            if(hour==12) {
-                degree = map.get(minutes);
-            }
-            else
-                degree = Math.abs(map.get(hour * 5) - map.get(minutes));
-        }
-        return Math.abs(degree-hourAffected);
+        if(hour == 12)
+            hour = 0;
+
+        double hourAngle = hour * 30 + minutes * 0.5;
+        double minuteAngle = minutes * 6;
+
+        double diff = Math.abs(hourAngle - minuteAngle);
+
+        return Math.min(diff, 360 - diff);
     }
 }
