4d1e32ea0d2c8168804cd97b1baaf1845db66b29
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-19 02:14:21 +0530

    LC-minimum-index-sum-of-two-lists [Runtime: N/A; Memory: N/A]

---
+++ string/minimum-index-sum-of-two-lists.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap<String,Integer> map=new HashMap<>();
+        int minSum=Integer.MAX_VALUE;
+        for(int i=0;i<list1.length;i++)
+            map.put(list1[i],i);
+
+        ArrayList<String> ans=new ArrayList<>();
+        int index=0;
+        for(String word: list2)
+        {
+            if(map.containsKey(word))
+            {
+
+                int sum= map.get(word)+index;
+                if(sum<=minSum) {
+                    ans.clear();
+                    ans.add(word);
+                    minSum=sum;
+                }
+            }
+            index++;
+        }
+        return ans.toArray(new String[0]);
+    }
+}
