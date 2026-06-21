f2f02e446adfd5498e8ab7ee774edd6ec6e530d8
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-21 22:54:58 +0530

    LC-maximum-ice-cream-bars [Runtime: N/A; Memory: N/A]

--- greedy/maximum-ice-cream-bars.java
+++ greedy/maximum-ice-cream-bars.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        int max=0,i=0,sum=0;
+        int max = 0, i = 0, sum = 0;
         Arrays.sort(costs);
-        if(costs[0]>coins) return 0;
+        if (costs[0] > coins) return 0;
 
-        while(max!=coins && i<=costs.length-1)
-        {
-            max+=costs[i];
-            sum++;i++;
+        while (i <= costs.length - 1) {
+            max += costs[i];
+            if(max<=coins)
+                sum++;
+            else break;
+            i++;
         }
         return sum;
-        
     }
 }
