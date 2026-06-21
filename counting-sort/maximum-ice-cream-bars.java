e7677aa6a996396a010a7b4e8abb804e66718c26
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-21 22:27:30 +0530

    LC-maximum-ice-cream-bars [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int max=0,i=0;
+        Arrays.sort(costs);
+        if(costs[0]>coins) return 0;
+        while(max!=coins && i<costs.length)
+        {
+            max+=costs[i];
+            i++;
+        }
+        return max;
+        
+    }
+}
