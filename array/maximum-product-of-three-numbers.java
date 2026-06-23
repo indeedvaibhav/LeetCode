57db8901309c485be8904bae278eb2a9ea05422e
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-23 15:09:55 +0530

    LC-maximum-product-of-three-numbers [Runtime: 3 ms; Memory: 47.3 MB]

--- array/maximum-product-of-three-numbers.java
+++ array/maximum-product-of-three-numbers.java
@@ -2,13 +2,38 @@ class Solution {
     public int maximumProduct(int[] nums) {
         if(nums.length<3) return -1;
 
-        Arrays.sort(nums);
-        int max=nums[nums.length-1];
-        int secondMax=nums[nums.length-2];
-        int thirdMax=nums[nums.length-3];
-        int min=nums[0];
-        int secondMin=nums[1];
+        int max=Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+        int thirdMax=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int secondMin=Integer.MAX_VALUE;
+        for(int i=0;i< nums.length;i++)
+        {
+            if (nums[i] > max) {
+                thirdMax = secondMax;
+                secondMax = max;
+                max = nums[i];
+            }
+            else if (nums[i] > secondMax) {
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            }
+            else if (nums[i] > thirdMax) {
+                thirdMax = nums[i];
+            }
 
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if (nums[i] < min) {
+                secondMin = min;
+                min = nums[i];
+            }
+            else if (nums[i] < secondMin) {
+                secondMin = nums[i];
+            }
+
+        }
         return Math.max(max*secondMax*thirdMax,min*secondMin*max);
         
 
