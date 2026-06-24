4c629cbffaef8f6c149b51b5a5a6621c3b3331c6
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-25 01:00:57 +0530

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

--- string/reverse-vowels-of-a-string.java
+++ string/reverse-vowels-of-a-string.java
@@ -1,24 +1,34 @@
 class Solution {
     public String reverseVowels(String s) {
-        ArrayList<Character> vowel= new ArrayList<>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='A'||s.charAt(i)=='E'|| s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'||s.charAt(i)=='a'||
-                s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+        ArrayList<Character> vowel = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' ||
+                s.charAt(i) == 'O' || s.charAt(i) == 'U' || s.charAt(i) == 'a' ||
+                s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
+                s.charAt(i) == 'u') {
+
                 vowel.add(s.charAt(i));
-        }
-        String ans="";int x=0;
-        for(int i=0;i<s.length();i++)
-        {
-
-            if(s.charAt(i)=='A'||s.charAt(i)=='E'|| s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'||s.charAt(i)=='a'||
-                    s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u') {
-
-                    ans += String.valueOf(vowel.get(vowel.size()-x-1));x++;
             }
-            else
-                ans+= String.valueOf(s.charAt(i));
         }
-        return ans;
+
+        StringBuilder ans = new StringBuilder();
+        int x = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' ||
+                s.charAt(i) == 'O' || s.charAt(i) == 'U' || s.charAt(i) == 'a' ||
+                s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
+                s.charAt(i) == 'u') {
+
+                ans.append(vowel.get(vowel.size() - x - 1));
+                x++;
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+
+        return ans.toString();
     }
 }
