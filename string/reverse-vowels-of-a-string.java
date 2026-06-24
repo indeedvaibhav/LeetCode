a37be927cc1085ca21f2492d536f039b11e92f0e
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-25 01:02:54 +0530

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

--- string/reverse-vowels-of-a-string.java
+++ string/reverse-vowels-of-a-string.java
@@ -1,7 +1,5 @@
 class Solution {
     public String reverseVowels(String s) {
-        class Solution {
-    public String reverseVowels(String s) {
         char[] arr = s.toCharArray();
 
         int left = 0;
@@ -28,7 +26,5 @@ class Solution {
 
     private boolean isVowel(char c) {
         return "AEIOUaeiou".indexOf(c) != -1;
-    }
-}
     }
 }
