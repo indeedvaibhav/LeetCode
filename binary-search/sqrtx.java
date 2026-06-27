fa91df4b29c5019269fb038df7bbb522011378a8
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-06-27 15:35:45 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ binary-search/sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 0) {
+            return Double.NaN; 
+        }
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        double guess = x / 2.0;
+        double precision = 0.000001; 
+        while (Math.abs(guess * guess - x) > precision) {
+            guess = (guess + (x / guess)) / 2.0;
+        }
+        return guess;
+    }
+    }
+}
