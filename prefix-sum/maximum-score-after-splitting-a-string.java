40a7ccc963e4c99896407146f8560b2df0f9c74f
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-08 04:45:30 +0530

    LC-maximum-score-after-splitting-a-string [Runtime: 20 ms; Memory: 46.4 MB]

--- prefix-sum/maximum-score-after-splitting-a-string.java
+++ prefix-sum/maximum-score-after-splitting-a-string.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxScore(String s) {
-        ArrayList<Integer> ans= new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        for(int i=0;i<s.length();i++)
-        {
-            int count=0,k=0;
-            String zeros= s.substring(0,i+1);
-            String ones= s.substring(i+1,s.length());
-            while(k<zeros.length() || k<ones.length())
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0, k = 0;
+            String zeros = s.substring(0, i + 1);
+            String ones = s.substring(i + 1, s.length());
+            if (zeros.length() > 0 && ones.length() > 0) 
             {
-                if(k<zeros.length())
+                while (k < zeros.length() || k < ones.length()) 
                 {
-                    if(zeros.charAt(k)=='0')
-                        count++;
+                    if (k < zeros.length()) {
+                        if (zeros.charAt(k) == '0')
+                            count++;
+                    }
+                    if (k < ones.length()) {
+                        if (ones.charAt(k) == '1')
+                            count++;
+                    }
+                    k++;
                 }
-                if(k<ones.length())
-                {
-                    if(ones.charAt(k)=='1')
-                        count++;
-                }
-                k++;
             }
             ans.add(count);
         }
         return Collections.max(ans);
-        
+
     }
 }
