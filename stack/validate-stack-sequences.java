8d6897faeb236bed00e344f3257d0d0d82b97123
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-10 14:30:27 +0530

    LC-validate-stack-sequences [Runtime: N/A; Memory: N/A]

--- stack/validate-stack-sequences.java
+++ stack/validate-stack-sequences.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-       Stack<Integer> stack= new Stack<>();
+      
+        Stack<Integer> stack= new Stack<>();
         int x=0;
-        for(int i=0;i<pushed.length;i++)
-
+        for(int i=0;i<=pushed.length-1;i++)
         {
             stack.push(pushed[i]);
-            if(stack.peek()==popped[x])
+            while(!stack.isEmpty() && stack.peek()==popped[x])
             {
-                stack.pop();x++;
+                stack.pop();
+                    x++;
             }
 
         }
-
-        for(;x<popped.length;x++)
-        {
-            if(stack.peek()==popped[x])
-                stack.pop();
-        }
         if(stack.isEmpty())
             return true;
-        else
-            return false;
+        return false;
 
         
     }
