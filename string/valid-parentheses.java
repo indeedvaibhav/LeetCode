09ef3dc40778fc16cd0b384086655bdd59673a14
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-12 00:57:05 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ string/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack= new Stack<>();
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+        for(int i=0;i<s.length();i++)
+        {
+            char ch= s.charAt(i);
+            if (map.containsKey(ch))
+            { stack.push(ch);
+            }
+            if (!stack.isEmpty() && (map.get(stack.peek()))==ch)
+            {
+                stack.pop();
+            }
+
+        }
+        if(stack.isEmpty())
+            return true;
+        return false;
+    }
+}
