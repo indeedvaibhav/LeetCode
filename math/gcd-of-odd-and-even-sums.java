2b3316b963551252bfe1021fa6a96025c26fecb1
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-16 00:14:03 +0530

    LC-gcd-of-odd-and-even-sums [Runtime: N/A; Memory: N/A]

---
+++ math/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int oddSum=0,evenSum=0,gcd=Integer.MIN_VALUE;
+        int odd=1,even=2;
+        while(n>=1)
+        {
+            evenSum+=even;
+            oddSum+=odd;
+            even+=2;
+            odd+=2;
+            n--;
+        }
+        for(int k=1;k<=9;k++)
+        {
+            if(evenSum%k==0 && oddSum%k==0)
+                gcd=Math.max(gcd,k);
+        }
+        return gcd;
+    }
+}
