ed61d91b38d021d6246c030ca982148c89b6854a
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-23 22:46:26 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-size-subarray-sum.java
+++ binary-search/minimum-size-subarray-sum.java
@@ -1,7 +1,10 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-         int sum = 0, min = Integer.MAX_VALUE, ans = 0;
+        if(nums.length==1 )
+            return (target==nums[0])?1:0;
+
+        int sum = 0, min = Integer.MAX_VALUE, ans = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             nums[i] = sum;
@@ -9,21 +12,25 @@ class Solution {
 
         int first = 0, last = nums.length - 1;
         for (int i = 0; i < nums.length; i++) {
-            if(first != last) {
+            if(first <= last) {
                 if ((nums[last] - nums[first]) == target) {
                     ans = last - first;
                     min = Math.min(min, ans);
                 }
+                if(first==last && nums[first]==target) {
+                    ans = last - first+1;
+                    min = Math.min(min, ans);
 
+                }
                 else if ((nums[last] - nums[first]) > target) {
                     ans = last - first;
                     min = Math.min(min, ans);
-                    if (nums[first] < target)
-                        first++;
-                    else
-                        last--;
-                }
 
+                }
+                if (nums[first] < target)
+                    first++;
+                else if(nums[last]>target)
+                    last--;
             }
             else
                 break;
@@ -33,5 +40,7 @@ class Solution {
             return 0;
         else
             return min;
+
+
     }
 }
