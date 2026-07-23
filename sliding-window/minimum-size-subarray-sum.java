e1cb5d53a507c3cf7194cae7d12075f8b460d872
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-07-23 22:20:44 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-size-subarray-sum.java
+++ sliding-window/minimum-size-subarray-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-        int sum = 0, min = Integer.MAX_VALUE;
+         int sum = 0, min = Integer.MAX_VALUE, ans = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             nums[i] = sum;
@@ -9,13 +9,25 @@ class Solution {
 
         int first = 0, last = nums.length - 1;
         for (int i = 0; i < nums.length; i++) {
-            if ((nums[last] - nums[first]) >= target) {
-                int ans = last - first;
-                min = Math.min(min, ans);
-            } else if (nums[first] < target)
-                first++;
+            if(first != last) {
+                if ((nums[last] - nums[first]) == target) {
+                    ans = last - first;
+                    min = Math.min(min, ans);
+                }
+
+                else if ((nums[last] - nums[first]) > target) {
+                    ans = last - first;
+                    min = Math.min(min, ans);
+                    if (nums[first] < target)
+                        first++;
+                    else
+                        last--;
+                }
+
+            }
             else
-                last--;
+                break;
+
         }
         if (min == Integer.MAX_VALUE)
             return 0;
