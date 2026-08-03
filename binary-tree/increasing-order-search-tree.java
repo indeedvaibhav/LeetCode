2e9724fc1b025431731c732191176d648b1345e9
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-03 11:45:52 +0530

    LC-increasing-order-search-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/increasing-order-search-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode pred=null;
+    TreeNode head=null;
+    public TreeNode increasingBST(TreeNode root) {
+        return inorder(root);
+        
+    }
+    public TreeNode inorder(TreeNode node)
+    {
+        if(node==null) return null;
+
+        inorder(node.left);
+        if(pred==null)
+        {
+            head=node;
+            head.left=null;
+        }
+        else
+            {
+                pred.right=node;
+                pred.left=null;
+            }
+            pred=node;
+
+        inorder(node.right);
+        return head;
+    }
+}
