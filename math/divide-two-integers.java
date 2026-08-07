b324ac168cb3f5afefc2efeb91e714efe74f514f
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-07 15:08:59 +0530

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

--- math/divide-two-integers.java
+++ math/divide-two-integers.java
@@ -1,6 +1,10 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        return dividend/divisor;
+        int i=1;
+        while(divisor<=dividend)
+{            divisor*=i;i++;
+}
+        return i;
         
     }
 }
