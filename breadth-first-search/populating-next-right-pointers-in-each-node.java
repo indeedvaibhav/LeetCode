9156486befe705389acf2929d2a5a2beccd419b5
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-08 14:57:14 +0530

    LC-populating-next-right-pointers-in-each-node [Runtime: N/A; Memory: N/A]

--- breadth-first-search/populating-next-right-pointers-in-each-node.java
+++ breadth-first-search/populating-next-right-pointers-in-each-node.java
@@ -1,17 +1,46 @@
-class Solution {
-  public Node connect(Node root) {
-    if (root == null)
-      return null;
-    connectTwoNodes(root.left, root.right);
-    return root;
-  }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
 
-  private void connectTwoNodes(Node p, Node q) {
-    if (p == null)
-      return;
-    p.next = q;
-    connectTwoNodes(p.left, p.right);
-    connectTwoNodes(q.left, q.right);
-    connectTwoNodes(p.right, q.left);
-  }
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null)
+            return null;
+        
+        Node leftMost=root;
+        while(leftMost.left!=null)
+        {
+            Node current= root;
+            while(current!=null)
+            {
+                current.left.next= current.right;
+                if(current.next!=null){
+                    current.right.next= current.next.left;
+                }
+                current= current.next;
+            }
+            leftMost=leftMost.left;
+        }
+        return root;
+        
+    }
 }
