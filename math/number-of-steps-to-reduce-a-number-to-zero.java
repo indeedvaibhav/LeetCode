f2b14c2e9f6c1fe17d1af1b322ca5e6a2287ad54
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-15 01:26:01 +0530

    LC-number-of-steps-to-reduce-a-number-to-zero [Runtime: N/A; Memory: N/A]

---
+++ math/number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int count=0;
+        while(num!=0)
+        {
+            if(num%2!=0)
+                num-=1;
+            else if(num%2==0)
+                num/=2;
+            count++;
+        }
+        return count;
+    }
+}
