29bf6439e7d149de6aafc453b697903fcbb8b418
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-23 03:34:18 +0530

    LC-sort-colors [Runtime: N/A; Memory: N/A]

--- bubble-sort/sort-colors.java
+++ bubble-sort/sort-colors.java
@@ -1,24 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] c= new int[3];
 
-        for(int i=0;i<nums.length;i++)
+        int[] c = new int[3];
+
+        // Count
+        for (int i = 0; i < nums.length; i++) {
             c[nums[i]]++;
-
-        for(int i =1 ;i<c.length;i++)
-            c[i]=c[i]+c[i-1];
-
-        int[] b= new int[nums.length];
-        for(int i=nums.length-1;i>=0;i--)
-        {
-            b[c[nums[i]]]= nums[i];
-            c[nums[i]]= c[nums[i]]-1;
         }
 
-        for(int i=0;i<nums.length;i++)
-        {
-            nums[i]=b[i];
+        // Put elements back into nums
+        int k = 0;
+
+        for (int i = 0; i < c.length; i++) {
+            while (c[i] > 0) {
+                nums[k] = i;
+                k++;
+                c[i]--;
+            }
         }
-        
     }
 }
