760efb30da29b6ad43a2fb3995c83e006142a2d8
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-26 01:07:16 +0530

    LC-smallest-missing-multiple-of-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/smallest-missing-multiple-of-k.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        int x =getMax(nums);
+        int ans=0;
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums)
+        {
+            set.add(num);
+        }
+        for(int i=1;i<x;i++)
+        {
+             int val= k*i;
+            if(!set.contains(val) && val%k==0)
+            {
+                ans= val;
+                break;
+            }
+            else 
+                continue;
+
+        }
+        
+        
+        return ans;
+    }
+    public int getMax(int[] arr)
+    {
+        int max =Integer.MIN_VALUE;
+        for(int num: arr)
+        {
+            if(num>max)
+                max=num;
+        }
+        return max;
+    }
+}
