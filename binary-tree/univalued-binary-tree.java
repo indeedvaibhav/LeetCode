86a90c7ab25cc885010262a3eef2fc743d659fd8
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-08-27 00:33:17 +0530

    LC-univalued-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/univalued-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root==null) return true;
+        int value= root.val;
+        return check(root,value);
+
+    }
+    public boolean check(TreeNode node , int value)
+    {
+        if(node==null) return true;
+        if(node.val!=value) return false;
+
+        return check(node.left,value) == check(node.right,value);
+        
+    }
+}
