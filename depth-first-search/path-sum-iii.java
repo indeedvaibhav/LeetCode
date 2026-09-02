0b74b947455aa759609567800f6696218d48aaa0
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-09-02 10:44:52 +0530

    LC-path-sum-iii [Runtime: N/A; Memory: N/A]

--- depth-first-search/path-sum-iii.java
+++ depth-first-search/path-sum-iii.java
@@ -17,6 +17,7 @@ class Solution {
     int count=0;
     public int pathSum(TreeNode root, int targetSum) {
         if(root==null) return 0;
+        if(targetSum==0) return 0;
         pathSum(root.left,targetSum);
         pathSum(root.right,targetSum);
 
