d59998cabf3a39f5e39d2578fc3f6356f8bc08eb
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-09-02 10:56:50 +0530

    LC-path-sum-iii [Runtime: N/A; Memory: N/A]

--- depth-first-search/path-sum-iii.java
+++ depth-first-search/path-sum-iii.java
@@ -1,41 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+
 class Solution {
-    int count=0;
+
+    int count = 0;
+
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null) return 0;
 
-        dfs(root,targetSum,0);
+        if (root == null) return 0;
 
-        pathSum(root.left,targetSum);
-        pathSum(root.right,targetSum);
+        dfs(root, targetSum, 0);
+
+        pathSum(root.left, targetSum);
+        pathSum(root.right, targetSum);
 
         return count;
     }
-    
-    public void dfs(TreeNode root, int targetSum,int currentSum){
 
-        if(root==null) return;
+    public void dfs(TreeNode root, int targetSum, long currentSum) {
 
-        
+        if (root == null) return;
+
         currentSum += root.val;
-        if(currentSum==targetSum) count++;
-        dfs(root.left,targetSum,currentSum);
-        dfs(root.right,targetSum,currentSum);
 
+        if (currentSum == targetSum)
+            count++;
+
+        dfs(root.left, targetSum, currentSum);
+        dfs(root.right, targetSum, currentSum);
     }
 }
-
