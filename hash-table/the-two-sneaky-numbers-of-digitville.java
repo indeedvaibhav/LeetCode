f10f001b29651c262a0d993346123437e9a6af9b
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-09-04 00:02:03 +0530

    LC-the-two-sneaky-numbers-of-digitville [Runtime: N/A; Memory: N/A]

---
+++ hash-table/the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> set =  new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put
+            (nums[i],map.getOrDefault(nums[i],0)+1);
+        }    
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.get(nums[i])>1)
+                set.add(nums[i]);
+        }
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
+        
+    }
+}
