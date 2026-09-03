94a6ff71b382ecb42c2f1f2cb1d6c2c42479000f
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-09-04 01:19:38 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- array/construct-uniform-parity-array-ii.java
+++ array/construct-uniform-parity-array-ii.java
@@ -1,59 +1,62 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        
-        boolean check = checkTrue(nums1);
-        if(check) return true;
-        boolean checkFalse= checkOdd(nums1);
-        if(checkFalse) return true;
-        int count=0;
-        for(int i = 0 ; i< nums1.length;i++){
 
-            if(nums1[i]%2!=0) 
+        boolean check = checkTrue(nums1);
+        if (check)
+            return true;
+        boolean checkFalse = checkOdd(nums1);
+        if (checkFalse)
+            return true;
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+
+            if (nums1[i] % 2 != 0) {
                 count++;
-            else{
-                for(int j =0 ;j<nums1.length;j++){
-                    if(j!=i && (nums1[i]-nums1[j])>=1)
-                    {
-                        if((nums1[i]-nums1[j])%2!=0) count++;
+            } else {
+                boolean possible = false;
+
+                for (int j = 0; j < nums1.length; j++) {
+                    if (j != i && (nums1[i] - nums1[j]) >= 1) {
+                        if ((nums1[i] - nums1[j]) % 2 != 0) {
+                            possible = true;
+                            break;
+                        }
                     }
                 }
+
+                if (possible)
+                    count++;
             }
         }
-        if(count==nums1.length) return true;
+        if (count == nums1.length)
+            return true;
         return false;
-        
-    }
-    public boolean checkTrue(int[] nums1){
-        int count=0;
 
-        for(int i=0;i<nums1.length;i++)
-        {
-            if(nums1[i]%2==0) 
+    }
+
+    public boolean checkTrue(int[] nums1) {
+        int count = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] % 2 == 0)
                 count++;
         }
-        if(count==nums1.length) return true;
+        if (count == nums1.length)
+            return true;
 
         return false;
     }
-    public boolean checkOdd(int[] nums1){
-        int count=0;
 
-        for(int i=0;i<nums1.length;i++)
-        {
-            if(nums1[i]%2!=0) 
+    public boolean checkOdd(int[] nums1) {
+        int count = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] % 2 != 0)
                 count++;
         }
-        if(count==nums1.length) return true;
+        if (count == nums1.length)
+            return true;
 
         return false;
     }
 }
-
-
-
-
-
-
-
-
-
