8af4245072778456904e4d8fe36ba8d217db2e34
indeedvaibhav <2k24.cs1q.2410345@gmail.com>
2026-09-11 14:22:57 +0530

    LC-lowest-common-ancestor-of-a-binary-search-tree [Runtime: N/A; Memory: N/A]

--- binary-lifting/lowest-common-ancestor-of-a-binary-search-tree.java
+++ binary-lifting/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -16,7 +16,7 @@ class Solution {
         if(curr.val < p.val && curr.val<q.val)
             return lowestCommonAncestor(curr.left, p,q);
         else if(curr.val > p.val && curr.val > q.val)
-            return lowestCommonAncestor(curr.left, p,q);
+            return lowestCommonAncestor(curr.right, p,q);
         else return curr;
         
     }
